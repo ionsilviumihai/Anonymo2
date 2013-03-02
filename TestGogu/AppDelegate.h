537232c4040788a43ec0a6e407ed5a7cb27fdee0
Meeshoo <meeshoo@Meeshoos-Mac.local>
2013-03-02 18:48:56 +0200

    Initial Commit

---
+++ TestGogu/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestGogu
+//
+//  Created by Meeshoo on 3/2/13.
+//  Copyright (c) 2013 Meeshoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TestGogu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestGogu
+//
+//  Created by Meeshoo on 3/2/13.
+//  Copyright (c) 2013 Meeshoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
